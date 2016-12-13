2d1edbe6184dc3ea0bad5ecfcbd550378cef146d
Mark Heiges <mheiges@uga.edu>
2016-12-13 17:15:17 -0500

    more options for removing puppet semodule

--- centos-7/scripts/removepuppet.sh
+++ centos-7/scripts/removepuppet.sh
@@ -6,4 +6,10 @@ rm -rf /var/lib/yum/repos/x86_64/7/puppet*
 rm -rf /opt/puppetlabs
 rm -rf /etc/puppetlabs
 
-/usr/sbin/semodule --remove=puppet
+if /usr/sbin/semodule --list-modules |grep -q puppet; then
+  if [[ -f /etc/selinux/targeted/modules/active/modules/puppet.pp ]]; then
+    /usr/sbin/semodule --remove=puppet
+  elif [[ -d /etc/selinux/targeted/active/modules/100/puppet ]]; then
+    rm -rf /etc/selinux/targeted/active/modules/100/puppet
+  fi
+fi

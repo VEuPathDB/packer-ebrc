3ece2543dcc818b0f621a44f42b5f2b324075d34
Mark Heiges <mheiges@uga.edu>
2017-04-07 13:55:47 -0400

    better comment re: devtimeout in /etc/sysconfig/network

--- centos-7/scripts/network_init_devtimeout.sh
+++ centos-7/scripts/network_init_devtimeout.sh
@@ -6,11 +6,15 @@
 # https://bugzilla.redhat.com/show_bug.cgi?id=1287726
 # https://bugzilla.redhat.com/show_bug.cgi?id=1194623
 
+this="$(basename $0)"
+
 if ! grep -q DEVTIMEOUT /etc/sysconfig/network; then
 cat >> /etc/sysconfig/network <<EOF
 
 # wait up to 10 seconds for NIC hardware detection before initializing
-# interface. -mheiges@uga.edu
+# interface. This is a Packer-managed addition using $this .
+# -mheiges@uga.edu
+#
 DEVTIMEOUT=10
 EOF
 fi

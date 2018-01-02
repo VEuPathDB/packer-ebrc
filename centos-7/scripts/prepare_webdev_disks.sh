8018a258f698eed07e31dda92e9baa0ecaed4708
Mark Heiges <mheiges@uga.edu>
2018-01-02 12:46:06 -0500

    change prepare_webdev_disks.sh to use virtio; stub in builder disk setup

--- centos-7/scripts/prepare_webdev_disks.sh
+++ centos-7/scripts/prepare_webdev_disks.sh
@@ -2,36 +2,48 @@
 # Format and mount external disks for web development VMs
 
 ######################################################################
+# Device naming depends on qemu interface used. /dev/sd* for sata
+# /dev/vd* for virtio. sata has a limit of 4 disks so we typical have 
+# to use virtio.
 
 # CONFIGURE
 APPDB_DIR=/u02/oradata/appdb
-APPDB_DEV=/dev/sdb
+APPDB_DEV=/dev/vdb
 APPDB_LABEL=appdb
 
 USERDB_DIR=/u02/oradata/userdb
-USERDB_DEV=/dev/sdc
+USERDB_DEV=/dev/vdc
 USERDB_LABEL=userdb
 
 ACCTDB_DIR=/u02/oradata/acctdb
-ACCTDB_DEV=/dev/sdd
+ACCTDB_DEV=/dev/vdd
 ACCTDB_LABEL=acctdb
 
 DATA_DIR=/var/www/Common/apiSiteFilesMirror
-DATA_DEV=/dev/sde
+DATA_DEV=/dev/vde
 DATA_LABEL=data
+
+BUILDER_DIR=/home/vmbuilder
+BUILDER_DEV=/dev/vdf
+BUILDER_LABEL=vmbuilder
+
 
 ######################################################################
 # FORMAT DISKS - full disk, no partition
+set -x
 mkfs.ext4 -F $APPDB_DEV
 mkfs.ext4 -F $USERDB_DEV
 mkfs.ext4 -F $ACCTDB_DEV
 mkfs.ext4 -F $DATA_DEV
+#mkfs.ext4 -F $BUILDER_DEV
 
 # LABEL DISKS
 e2label $APPDB_DEV   $APPDB_LABEL
 e2label $USERDB_DEV  $USERDB_LABEL
 e2label $ACCTDB_DEV  $ACCTDB_LABEL
 e2label $DATA_DEV    $DATA_LABEL
+#e2label $BUILDER_DEV $BUILDER_LABEL
+set +x
 
 # # MAKE MOUNTPOINTS
 mkdir -p $APPDB_DIR
@@ -46,6 +58,7 @@ LABEL=$APPDB_LABEL $APPDB_DIR ext4 nofail,defaults 0  0
 LABEL=$USERDB_LABEL $USERDB_DIR ext4 nofail,defaults 0  0
 LABEL=$ACCTDB_LABEL $ACCTDB_DIR ext4 nofail,defaults 0  0
 LABEL=$DATA_LABEL $DATA_DIR ext4 nofail,defaults 0  0
+#LABEL=$BUILDER_LABEL $BUILDER_DIR ext4 nofail,defaults 0  0
 EOF
 
 # MOUNT ALL
